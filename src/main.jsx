9d2a51d56cd53924a67c59ea282782ec70a47dbd
ARUNKUMARP123 <arun841988@gmail.com>
2024-06-14 21:13:57 +0530

    to responsive

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ createRoot(document.getElementById('root')).render(
       
       <Router>
       <CssBaseline />
-      <Container maxWidth="sm">
+      <Container maxWidth="lg">
         <ErrorBoundary>
           <Routes>
             <Route path="*" element={<App baseUrl={baseUrl} />} />
